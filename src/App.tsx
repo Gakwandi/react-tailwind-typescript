86f08d8b1ebc410b5a351d5358c2e0872468a5dd
Gakwandi <gakwandijoshua@gmail.com>
2021-01-10 18:34:47 +0200

    Downgraded autoprefix from ^10 to ^9

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+      <h1>Super cool page</h1>
+      <div className="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
+      </div>
     </div>
   );
 }
